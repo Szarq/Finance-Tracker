2a720dffae28315e372ab74ba6b7f2757dc16ca3
Artur <artur040493@gmail.com>
2017-02-02 21:24:43 +0000

    User stock listing correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def my_portfolio
     @user = current_user
-    @user_stocks = current_user.stocks
+    @user_stocks = current_user.user_stocks
   end
   
   def my_friends
